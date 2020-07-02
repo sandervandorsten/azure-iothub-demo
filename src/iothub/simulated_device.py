464534c9c08a8965bee6bb6a8a344eaeacc831cf
Sander van Dorsten <sandervandorsten@gmail.com>
2020-07-02 12:24:37 +0200

    send fan activity to cloud

--- src/iothub/simulated_device.py
+++ src/iothub/simulated_device.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 import datetime
+import json
 import random
 import time
 import threading
@@ -23,7 +24,6 @@ TELEMETRY_INTERVAL = 3
 # Define the JSON message to send to IoT Hub.
 TEMPERATURE = 20.0
 HUMIDITY = 60
-MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'
 
 # time in seconds that the fan should stay on if it is too warm.
 FAN_SESSION_DURATION = 10
@@ -147,10 +147,13 @@ def iothub_client_telemetry_sample_run():
 
 
 def send_message(client: IoTHubDeviceClient):
+    global FAN_ACTIVE
     # Build the message with simulated telemetry values.
     temperature = TEMPERATURE + (random.random() * 15)
     humidity = HUMIDITY + (random.random() * 20)
-    msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
+    msg_txt_formatted = json.dumps(
+        {"temperature": temperature, "humidity": humidity, "fan_active": FAN_ACTIVE}
+    )
     message = Message(msg_txt_formatted)
     # Add a custom application property to the message.
     # An IoT hub can filter on these properties without access to the message body.
